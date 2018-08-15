064ebe30634ed337286248097c09cab192eb611b
spsanderson <spsanderson@gmail.com>
2018-08-15 15:07:25 -0400

    comparison operators

---
+++ comparison_operators.R
@@ -0,0 +1,23 @@
+# Comparison Operators
+5 > 6 # should return FALSE
+5 < 6 # Should return TRUE
+5 <= 6 # Should return TRUe
+
+# Equality
+2 == 3 # will return false
+
+# Inequality
+2 != 4 # will return TRUE
+
+# Watch your spacing!!!
+v < -1 # will check for boolean
+v <-1  # will reassign
+
+# Vector comparisons
+v <- c(1,2,3,4,5)
+v < 2
+v == 3
+
+# Element by Element for multiple vectors
+v2 <- c(10,20,30,40,50)
+v < v2
