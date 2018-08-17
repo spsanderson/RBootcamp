b9187657725f893671fa1ac5564fe8061129e250
spsanderson <spsanderson@gmail.com>
2018-08-17 15:15:16 -0400

    data.frame operations

--- data_frames.R
+++ data_frames.R
@@ -53,3 +53,54 @@ df[desc.temp, ]
 
 desc.temp <- order(-df$temp)
 df[desc.temp, ]
+
+#####
+# Data Frame Operations
+# Creating Data Frames
+empty <- data.frame()
+c1 <- 1:10
+c2 <- letters[1:10]
+c1
+c2
+df <- data.frame(col.name.1 = c1, col.name.2 = c2)
+df
+
+# Import export
+d2 <- read.csv('some_file.csv')
+write.csv(df, file = 'saved_df.csv') # index saved as column X
+df2 <- read.csv('saved_df.csv')
+df2
+
+# Getting info about df
+nrow(df) # how many rows do you have
+ncol(df) # how many columns less the idnex column
+colnames(df) # column names
+str(df)
+summary(df)
+
+# Reference a particular cell in a data.frame
+df
+df[[5,2]] # cell at row 5 column 2 'e'
+df[5,2]
+a <- df[[5,2]]
+b <- df[5,2]
+str(a)
+str(b)
+df[[5,'col.name.2']]
+df[[2,'col.name.1']] <- 9999
+df
+df[1,] # return a row - returns data.frame not vector
+
+# Reference columns
+head(mtcars)
+nrow(mtcars)
+
+mtcars$mpg # most common way to grab a column returns a vector
+mtcars[, 'mpg'] # returns a vector
+mtcars[, 1] # returns a vector
+mtcars[['mpg']] # again returns a vector
+mtcars['mpg'] #returns a data.frame
+mtcars[1] # returns a data.frame
+head(mtcars[c('mpg', 'cyl')]) # get multiple columns
+
+# Overview data frame operations
