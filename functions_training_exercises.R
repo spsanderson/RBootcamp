f12db43a7a2e82c2d7c0436ebf0aaa6eb02a1e1d
spsanderson <spsanderson@gmail.com>
2018-08-22 15:24:16 -0400

    implement a function using a counter, bring return(FALSE) to outside of for loop for int_in_vector

--- functions_training_exercises.R
+++ functions_training_exercises.R
@@ -30,10 +30,33 @@ int_in_vector <- function(int1, vec1) {
   for(num in vec1){
     if(int1 == vec1[num]){
       return(TRUE)
-    } else {
-      return(FALSE)
     }
   }
+  return(FALSE)
 }
 int_in_vector(4, c(1,2,3))
 
+# Ex 3: Create a function that accepts two arguments, an integer and a vector 
+# of integers. It returns the count of the number of occurences of the integer 
+# in the input vector. 
+a <- 1
+b <- c(1,1,2)
+
+count_func <- function(a,b) {
+  count = 0
+  for(item in b){
+    if(a == item){
+      count = count + 1
+    }
+  }
+  return(count)
+}
+count_func(1,c(1,1,1,1,1,1,1,1,1))
+
+# Ex 4: We want to ship bars of aluminum. We will create a function that accepts
+# an integer representing the requested kilograms of aluminum for the package to
+# be shipped. To fullfill these order, we have small bars (1 kilogram each) and 
+# big bars (5 kilograms each). Return the least number of bars needed.
+# For example, a load of 6 kg requires a minimum of two bars 
+# (1 5kg bars and 1 1kg bars). A load of 17 kg requires a minimum of 5 bars 
+# (3 5kg bars and 2 1kg bars). 
