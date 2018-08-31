97f76f8d956c096cf94b02831b0d18cefd1628ec
spsanderson <spsanderson@gmail.com>
2018-08-31 14:18:58 -0400

    distinct, trans(mutate), summarise, sample_n, sample_frac

--- data_manipulation.R
+++ data_manipulation.R
@@ -51,3 +51,14 @@ head(
     , airline_carrier = carrier
   )
 )
+
+distinct(select(flights, carrier))
+
+head(mutate(flights, new_col = (arr_delay - dep_delay)))
+head(transmute(flights, new_col = (arr_delay - dep_delay)))
+
+summarise(flights, avg_air_time = mean(air_time, na.rm = T))
+summarise(flights, total_time = sum(air_time, na.rm = T))
+
+sample_n(flights, 10)
+sample_frac(flights, 0.05)
