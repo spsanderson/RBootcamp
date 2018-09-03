3f47323c372cc6bc87881d2e79fe49ff584a8bd4
spsanderson <spsanderson@gmail.com>
2018-09-03 13:26:29 -0400

    bar plot

--- data_viz.R
+++ data_viz.R
@@ -58,3 +58,28 @@ pl1 <- pl + geom_point(
 pl1 <- pl1 + scale_color_gradient(low = "blue", high = "red")
 # Print graph
 pl1
+
+# Bar Plots
+df <- mpg
+# Data & Aestetics
+pl1 <- ggplot(
+  data = df
+  , aes(
+    x = class
+  )
+)
+# Geometries
+pl1 <- pl1 + geom_bar(
+  aes(
+    fill = drv
+  )
+  #, position = position_dodge(preserve = "total")
+  #, position = "dodge"
+  , position = "fill"
+)
+# Facets
+# Statistics
+# Coordinates
+# Theme
+# Print Graph
+pl1
