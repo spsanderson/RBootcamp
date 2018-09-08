9f43634bf3da98735135e51dfc794df96e7c8b73
spsanderson <spsanderson@gmail.com>
2018-09-08 11:06:04 -0400

    lib load, batting file read in, BA and OBP calculated.

---
+++ money_ball_capstone.R
@@ -0,0 +1,24 @@
+# Money Ball Capstone
+# Lib Load ####
+library(dplyr)
+library(ggplot2)
+
+# Get File ####
+file.to.load <- file.choose(new = T)
+batting <- read.csv(file.to.load)
+rm(file.to.load)
+
+# look at data
+head(batting)
+str(batting)
+head(batting$AB, 5)
+head(batting$X2B, 5)
+
+# Feature Engineering ####
+# Add Batting Average
+batting$BA <- batting$H / batting$AB
+tail(batting$BA)
+
+batting$OBP <- ( batting$H + batting$BB + batting$HBP ) / 
+  ( batting$AB + batting$BB + batting$HBP + batting$SF )
+head(batting$OBP)
