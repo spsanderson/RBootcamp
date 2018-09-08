845d122342eb4566518026d845a341b8cb83437e
spsanderson <spsanderson@gmail.com>
2018-09-08 14:56:33 -0400

    add funmodeling lib

--- money_ball_capstone.R
+++ money_ball_capstone.R
@@ -2,11 +2,16 @@
 # Lib Load ####
 library(dplyr)
 library(ggplot2)
+library(funModeling)
 
 # Get File ####
 file.to.load <- file.choose(new = T)
 batting <- read.csv(file.to.load)
-rm(file.to.load)
+
+df_status(batting)
+profiling_num(batting)
+describe(batting)
+#rm(file.to.load)
 
 # look at data
 head(batting)
@@ -17,6 +22,10 @@ head(batting$X2B, 5)
 # Feature Engineering ####
 # Add Batting Average
 batting$BA <- batting$H / batting$AB
+batting <- batting %>%
+  filter(AB > 0) %>%
+  mutate(BA2 = H / AB)
+  
 tail(batting$BA)
 
 head(batting)
