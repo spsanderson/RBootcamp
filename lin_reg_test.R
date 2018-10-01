e7521e07052b4227e23c4a32d5736948da82274d
spsanderson <spsanderson@gmail.com>
2018-10-01 15:14:19 -0400

    add some correlation plots. Build linear model.

--- lin_reg_test.R
+++ lin_reg_test.R
@@ -1,5 +1,4 @@
 # Lin Reg Lecture ####
-
 # Read in data ####
 File.To.Choose <- file.choose(new = T)
 df <- read.csv(File.To.Choose, sep = ';')
@@ -30,5 +29,47 @@ cor.data
 
 corrplot(cor.data)
 corrplot(cor.data, method = 'color')
+corrplot(cor.data, method = 'circle')
+corrplot(cor.data, method = 'pie')
 cor.p.test <- cor.mtest(cor.data)
 corrplot(cor.p.test[["p"]])
+corrplot(cor.p.test[["p"]], method = 'pie')
+
+corrgram(df)
+corrgram(
+  df
+  , order = T
+  , lower.panel = panel.shade
+  , upper.panel = panel.pie
+  , text.panel = panel.txt
+)
+
+ggplot(df, aes(x = G3)) +
+  geom_histogram(
+    bins = 20
+    , alpha = 0.5
+    , fill = 'blue'
+  )
+
+# Build a model ####
+library(caTools) # to slipt into test/train data
+# Set a seed
+set.seed(101)
+# Split sample
+sample <- sample.split(df$G3, SplitRatio = 0.7)
+# Train
+train <- subset(df, sample == TRUE)
+# Test
+test <- subset(df, sample == FALSE)
+# Build Model on Training data
+model <- lm(G3 ~., data = train) # all features
+print(summary(model))
+# Residuals
+res <- residuals(model)
+res <- as.data.frame(res)
+head(res, 5)
+ggplot(data = res, aes(res)) +
+  geom_histogram(
+    fill = 'blue'
+    , alpha = 0.5
+  )
