f482fd1161174fca4ed73ecc8a62eeb8ca8f5a6b
Steven Sanderson <spsanderson@gmail.com>
2019-04-27 15:47:48 -0400

    make test/train data

--- knn_project.R
+++ knn_project.R
@@ -14,3 +14,22 @@ install.load::install_load(
 
 # Get file ####
 # use iris dataset
+df <- datasets::iris
+head(df, 5)
+str(df)
+
+# Standardize df
+Species <- df$Species
+df <- df[, -5]
+df.std <- scale(df)
+str(df)
+var(df.std[,1])
+var(df.std[,2])
+df.final <- data.frame(df.std, Species)
+head(df.final, 2)
+
+# Train / Test Data ####
+split <- sample.split(df.final$Species, SplitRatio = 0.7)
+
+train.data <- subset(df, split == T)
+test.data <- subset(df, split == F)
