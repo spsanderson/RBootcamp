568d263d687ab731d916987704872bf670f754ca
Steven Sanderson <spsanderson@gmail.com>
2019-04-28 13:47:19 -0400

    updates

--- tree_methods_lecture.R
+++ tree_methods_lecture.R
@@ -30,33 +30,3 @@ rf <- randomForest(Kyphosis ~ ., data = df)
 print(rf)
 importance(rf)
 
-# Use MLR ####
-# Make train and test tasks
-trainTask <- makeClassifTask(data = train, target = "income")
-testTask <- makeClassifTask(data = test, target = "income")
-
-# mlr Tree ####
-getParamSet("classif.rpart")
-
-# Make learner tree
-learner.tree <- makeLearner('classif.rpart', predict.type = 'response')
-
-# make CV
-tree.cv <- makeResampleDesc("CV", iters = ncol(df))
-
-# Hyper-parameter search - grid search
-tree.gs <- makeParamSet(
-  makeIntegerParam('minsplit', lower = 2, upper = 50)
-  , makeIntegerParam('minbucket', lower = 2, upper = 50)
-  , makeIntegerParam('cp', lower = 0.001, upp = 0.2)
-)
-
-# grid search
-tree.gs.control <- makeTuneControlGrid()
-
-# tune hyper-parameters
-tree.tune <- tuneParams(
-  learner = learner.tree
-  , resampling = tree.cv
-  , task = trainTask
-)
